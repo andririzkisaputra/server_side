078eb27335507113f4eaeffcba46cf06cb99133e
andririzkisaputra <andri.rizki007@gmail.com>
2021-08-08 23:48:18 +0700

    detail produk

--- application/models/Produk_model.php
+++ application/models/Produk_model.php
@@ -36,6 +36,14 @@ class Produk_model extends CI_Model {
   public function get_produk_detail() {
     $data = $this->db->from('produk')->where(['produk_id' => $this->where['produk_id']])->get()->result();
     foreach ($data as $key => $value) {
+      $varian_id = json_decode($value->varian_id);
+      if (count($varian_id) > 1) {
+        foreach ($varian_id as $k => $v) {
+          $value->varian[]   = $this->db->from('varian')->where(['varian_id' => $v])->get()->row();
+        }
+      }else {
+        $value->varian[]     = $this->db->from('varian')->where(['varian_id' => $varian_id])->get()->row();
+      }
       $value->harga_produk_f = $this->all_library->format_harga($value->harga_produk);
       $value->link_gambar    = base_url().'assets/produk/'.$value->gambar;
     }
@@ -121,6 +129,7 @@ class Produk_model extends CI_Model {
       'varian'       => $this->data['varian'],
       'sub_varian'   => json_encode($this->data['sub_varian']),
       'harga_varian' => json_encode($this->data['harga_varian']),
+      'jenis_varian' => $this->data['jenis_varian'],
       'is_deleted'   => '1',
       'modified_on'  => date('Y-m-d H:i:s')
     );
@@ -143,10 +152,62 @@ class Produk_model extends CI_Model {
       'varian'       => $this->data['varian'],
       'sub_varian'   => json_encode($this->data['sub_varian']),
       'harga_varian' => json_encode($this->data['harga_varian']),
+      'jenis_varian' => json_encode($this->data['jenis_varian']),
       'modified_on'  => date('Y-m-d H:i:s')
     );
     $this->db->where('varian_id', $this->where['varian_id']);
     $update = $this->db->update('varian', $data);
+    return $this->result['success'] = ($update) ? true : false;
+  }
+
+  public function set_bahanBaku() {
+    $data = array(
+      'bahan_baku'   => $this->data['bahan_baku'],
+      'is_deleted'   => '1',
+      'modified_on'  => date('Y-m-d H:i:s')
+    );
+    $insert = $this->db->insert('bahan_baku', $data);
+    return $this->result['success'] = ($insert) ? true : false;
+  }
+
+  public function get_bahan_baku() {
+    $data     = $this->bahan_baku('result');
+    $num_rows = $this->bahan_baku('num_rows');
+    $this->result['data']      = $data;
+    $this->result['totaldata'] = $num_rows;
+    return $this->result;
+  }
+
+  public function bahan_baku($get) {
+    $this->db->from('bahan_baku');
+    $this->db->where('is_deleted', '1');
+    if (isset($this->where['search'])) {
+      $this->db->like('bahan_baku', $this->where['search']);
+    }
+    if ($get == 'result') {
+      $this->db->limit($this->perpage, $this->page);
+    }
+    $data = $this->db->get()->$get();
+    return $data;
+  }
+
+  public function hapus_bahan_baku() {
+    $data = array(
+      'is_deleted'  => '0',
+      'modified_on' => date('Y-m-d H:i:s')
+    );
+    $this->db->where('bahan_baku_id', $this->where['bahan_baku_id']);
+    $update = $this->db->update($this->table, $data);
+    return $this->result['success'] = ($update) ? true : false;
+  }
+
+  public function ubah_bahan_baku() {
+    $data = array(
+      'bahan_baku'   => $this->data['bahan_baku'],
+      'modified_on'  => date('Y-m-d H:i:s')
+    );
+    $this->db->where('bahan_baku_id', $this->where['bahan_baku_id']);
+    $update = $this->db->update('bahan_baku', $data);
     return $this->result['success'] = ($update) ? true : false;
   }
 
